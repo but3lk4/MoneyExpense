11bb7ff00d4c3be5c2a3b2cf4b00d506ca66c6e6
kubson <kuba.haladus@gmail.com>
2019-02-14 22:12:40 +0100

    Dodaj pliki projektów.

---
+++ MoneyExpense/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoneyExpense.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
